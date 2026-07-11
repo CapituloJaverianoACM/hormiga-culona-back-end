edba826d341722ebdeacb919640a26186a0c5b63
Dreamerend3008 <79518618+Dreamerend3008@users.noreply.github.com>
2026-07-11 16:23:32 -0500

    agents classes and tests

---
+++ AI/open_ai_client.py
@@ -0,0 +1,13 @@
+import asyncio
+import os
+import dotenv
+from agent_framework import Agent
+from agent_framework_openai import OpenAIChatClient 
+
+class OpenAiClient(OpenAIChatClient):
+    def __init__(self):
+        dotenv.load_dotenv()
+        self.base_url = os.getenv("FOUNDRY_PROJECT_ENDPOINT")
+        self.model = os.getenv("FOUNDRY_MODEL_DEPLOYMENT_NAME")
+        self.api_key = os.getenv("AZURE_AI_FOUNDRY_API_KEY")
+        super().__init__(base_url=self.base_url, model=self.model, api_key=self.api_key)
