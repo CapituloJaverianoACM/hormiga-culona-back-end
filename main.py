ae9803b3d3921d81f3961169e7d93398dca10116
SalomonAvila <salomon.avila2506@gmail.com>
2026-07-11 14:26:21 -0500

    implementado el health check

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 # Here goes fastapi ports and init
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI(
+    title="Hormiga Culona APIII",
+    description="Backend para el empalme entre el front y el agente",
+    version="1.0.0"
+)
+
+# Despues se añadira el CORS si desplegamos
+# app.add_middleware()
+
+@app.get("/ping")
+def healthMonitor():
+    return {"status" : "ok", "message" : "pong"}
 
 
